250fcfd71fbcd98fa92d4fc891c875db363f8981
Sara <saralicciardello1972@gmail.com>
2026-07-02 23:59:58 +0200

    aggiunto comando hotdog

--- bot.py
+++ bot.py
@@ -38,8 +38,30 @@ async def chips(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Patata x1> Tagliere > Chips x4\n"
         "Chips > Friggitrice > Chips Cotte\n\n"
         "» Chips alla paprika:\n"
-        "Chips Cotte x1+ Paprika x1> Chips alla Paprika x2\n\n"
+        "Chips Cotte x1 + Paprika x1 > Chips alla Paprika x2\n\n"
         "Da usare sul banco da lavoro."
+    )
+
+    await update.message.reply_text(text, parse_mode="Markdown")
+
+
+async def hotdog(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = (
+        "🌭 *Eccoti gli Hotdog:*\n\n"
+        "» HotDog classico:\n"
+        "x1 Pane per hotdog + x1 Wurstel cotto + x1 Maionese + x1 Ketchup › Banco da Lavoro\n\n"
+        "» HotDog Vegano:\n"
+        "x1 Pane per hotdog + x1 Wurstel veg cotto + x1 Lattuga + x1 Pomodoro › Banco da Lavoro > x4 Hot dog veg\n\n"
+        "» HotDog con Cipolla Croccante:\n"
+        "x1 Pane per hotdog + x1 Wurstel cotto + x1 Cipolle fritte + x1 Senape › Banco da lavoro > HotDog con Cipolla x4\n\n"
+        "Il pane per hotdog si ottiene tagliando pane per hamburger su un tagliere con il coltello."
+    )
+
+    await update.message.reply_text(text, parse_mode="Markdown")
+
+async def pasta(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = (
+        ""
     )
 
     await update.message.reply_text(text, parse_mode="Markdown")
@@ -51,5 +73,8 @@ app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("cucina", cucina))
 app.add_handler(CommandHandler("chips", chips))
+app.add_handler(CommandHandler("hotdog", hotdog))
+app.add_handler(CommandHandler("pasta", pasta))
 
 app.run_polling()
+
